7beb43256a29183957fd533db8477e9efe9aa763
Drew0004 <drdrewgraphic@gmail.com>
2024-10-03 18:37:14 +0200

    Add About section - responsive to be done

--- src/components/ClaimSection.jsx
+++ src/components/ClaimSection.jsx
@@ -3,7 +3,7 @@ import { Square, Triangle, Circle, Star, Bars, ArrowRightDown} from '../assets/i
 
 const ClaimSection = () => {
   return (
-    <div className='img-responsive-claim-lg img-responsive-claim-md img-responsive-claim d-flex justify-content-center align-items-end' style={{ minHeight:'700px' }}>
+    <div className='img-responsive-claim-lg img-responsive-claim-md img-responsive-claim d-flex justify-content-center align-items-end min-h-700'>
         <h4 className='text-white text-decoration-underline text-uppercase fw-bold c-pointer mb-3'>
             // About me <ArrowRightDown width={20} height={20} className={'fill-white ms-3'}/>
         </h4>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ClaimSection from '../ClaimSection'
+import AboutSection from '../AboutSection'
 
 const Home = () => {
   return (
     <>
         <ClaimSection/>
+        <AboutSection/>
     </>
   )
 }
