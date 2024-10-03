570202f063f65b39e0cd794123d28a9ef124660f
Drew0004 <drdrewgraphic@gmail.com>
2024-10-03 17:29:01 +0200

    Fix claim section responsive

--- src/components/ClaimSection.jsx
+++ src/components/ClaimSection.jsx
@@ -3,42 +3,10 @@ import { Square, Triangle, Circle, Star, Bars, ArrowRightDown} from '../assets/i
 
 const ClaimSection = () => {
   return (
-    <div className='main-red-bg'>
-        <div className="row align-items-end g-0">
-            {/* left side  */}
-            <div className="col-6 d-flex flex-wrap my-5">
-
-                {/* black stripes */}
-                <div className='bg-black my-1 col-12' style={{ height: '60px' }}></div>
-                <div className='bg-black my-1 col-12' style={{ height: '30px' }}></div>
-                <div className='bg-black my-1 col-12' style={{ height: '15px' }}></div>
-                <div className='bg-black my-1 col-12' style={{ height: '10px' }}></div>
-                {/* shapes */}
-                <div className="col-6 mt-5">
-                    <div className="container-fluid">
-                        <Star height={150} className={'m-2'}/>
-                        <Triangle height={150} className={'m-2'}/>
-                        <Square height={150} className={'m-2'}/>
-                        <Circle height={150} className={'m-2'}/>
-                    </div>
-                </div>
-
-                {/* vertical bars */}
-                <div className="col-6 mt-5 text-end">
-                    <Bars height={300}/>
-                </div>
-
-
-                <h4 className='text-white text-decoration-underline text-uppercase my-5 ms-auto fw-bold c-pointer'>
-                    // About me <ArrowRightDown width={20} height={20} className={'fill-white ms-3'}/>
-                </h4>
-            </div>
-
-            {/* right side */}
-            <div className="col-6 mt-5 h-100">
-                <img style={{ objectPosition: 'right' }} className='w-100 h-100 object-fit-contain'  src="/img/Astronaut.png" alt="Astronaut" />
-            </div>
-        </div>
+    <div className='img-responsive-claim-lg img-responsive-claim-md img-responsive-claim d-flex justify-content-center align-items-end' style={{ minHeight:'700px' }}>
+        <h4 className='text-white text-decoration-underline text-uppercase fw-bold c-pointer mb-3'>
+            // About me <ArrowRightDown width={20} height={20} className={'fill-white ms-3'}/>
+        </h4>
     </div>
   )
 }
