fd43e15ab4be84bbbb115d1d1e550b9a1d3683d9
Drew0004 <drdrewgraphic@gmail.com>
2024-10-07 11:30:28 +0200

    Fix swiper responsive

--- src/components/ProjectsSlider.jsx
+++ src/components/ProjectsSlider.jsx
@@ -20,16 +20,24 @@ const ProjectSlider = forwardRef((props, worksRef) => {
         }}
         modules={[Autoplay, Pagination]}
         loop={true}
-        pagination={{ type : 'bullets'}}
+        pagination={
+            { 
+                type : 'bullets',
+                clickable: true
+            }
+        }
         className="mySwiper">
         {
           projects.map((slide, slideIndex) => (
               <SwiperSlide key={slideIndex}>
                 <div className='min-h-200 min-h-md-500 min-h-lg-700 slider-img' style={{ backgroundImage: `url('${slide.mainImg}')`}}>
-                    <div className="container-fluid">
-                        <h2 className='fw-bold text-uppercase main-red primary-titles'>
+                    <div className="container-fluid d-flex justify-content-between align-items-center">
+                        <h2 className='fw-bold text-uppercase main-red secondary-titles'>
                             // 0 {slideIndex + 1}
                         </h2>
+                        <button className="carousel-btn px-3 py-1 px-md-4 py-md-2 d-flex align-items-center">
+                            <span className='fw-bold me-4'>View Project</span> <ArrowRightUp width={'20px'} height={'20px'}/>
+                        </button>
                     </div>
                 </div>
               </SwiperSlide>
