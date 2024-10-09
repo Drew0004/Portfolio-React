2c739fed86e2e3816b452cc4f39f827eaa864a83
Drew0004 <drdrewgraphic@gmail.com>
2024-10-09 14:37:43 +0200

    Fix scroll position

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import MyHeader from '../MyHeader';
 import Footer from '../Footer';
 import AboutProject from '../AboutProject';
 import ProjectFunctionality from '../ProjectFunctionality';
-
+import { useLocation } from 'react-router-dom'
 
 const Project = () => {
+    const {pathname} = useLocation()
+
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[pathname])
+    
     return (
         <>
             <MyHeader/>
